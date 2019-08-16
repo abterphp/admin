cf67214bc65cfbf5a82485f6dee6a8d4b760f572
Peter Aba <peter@peteraba.com>
2019-08-16 04:01:23 +0200

    Add more repo tests

---
+++ tests/Orm/TokenRepoTest.php
@@ -0,0 +1,169 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AbterPhp\Admin\Orm;
+
+use AbterPhp\Admin\Domain\Entities\Token as Entity;
+use Opulence\Orm\DataMappers\IDataMapper;
+use Opulence\Orm\IEntityRegistry;
+use Opulence\Orm\IUnitOfWork;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+class TokenRepoTest extends TestCase
+{
+    /** @var ApiClientRepo - System Under Test */
+    protected $sut;
+
+    /** @var string */
+    protected $className = 'Foo';
+
+    /** @var IDataMapper|MockObject */
+    protected $dataMapperMock;
+
+    /** @var IUnitOfWork|MockObject */
+    protected $unitOfWorkMock;
+
+    public function setUp(): void
+    {
+        $this->dataMapperMock = $this->getMockBuilder(IDataMapper::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['add', 'delete', 'getAll', 'getById', 'update', 'getByClientId'])
+            ->getMock();
+
+        $this->unitOfWorkMock = $this->getMockBuilder(IUnitOfWork::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods([
+                'commit',
+                'detach',
+                'dispose',
+                'getEntityRegistry',
+                'registerDataMapper',
+                'scheduleForDeletion',
+                'scheduleForInsertion',
+                'scheduleForUpdate',
+                'setConnection',
+            ])
+            ->getMock();
+
+        $this->sut = new TokenRepo($this->className, $this->dataMapperMock, $this->unitOfWorkMock);
+    }
+
+    public function testGetAll()
+    {
+        $entityStub0 = new Entity('foo0', 'foo-0', null, null);
+        $entityStub1 = new Entity('foo1', 'foo-1', null, null);
+        $entities    = [$entityStub0, $entityStub1];
+
+        $entityRegistry = $this->createEntityRegistryStub(null);
+
+        $this->dataMapperMock->expects($this->once())->method('getAll')->willReturn($entities);
+
+        $this->unitOfWorkMock->expects($this->any())->method('getEntityRegistry')->willReturn($entityRegistry);
+
+        $actualResult = $this->sut->getAll();
+
+        $this->assertSame($entities, $actualResult);
+    }
+
+    public function testGetByIdFromCache()
+    {
+        $entityStub = new Entity('foo0', 'foo-0', null, null);
+
+        $entityRegistry = $this->createEntityRegistryStub($entityStub);
+
+        $this->unitOfWorkMock->expects($this->any())->method('getEntityRegistry')->willReturn($entityRegistry);
+
+        $this->dataMapperMock->expects($this->never())->method('getById');
+
+        $id = 'foo';
+
+        $actualResult = $this->sut->getById($id);
+
+        $this->assertSame($entityStub, $actualResult);
+    }
+
+    public function testGetByIdFromDataMapper()
+    {
+        $entityStub = new Entity('foo0', 'foo-0', null, null);
+
+        $entityRegistry = $this->createEntityRegistryStub(null);
+
+        $this->unitOfWorkMock->expects($this->any())->method('getEntityRegistry')->willReturn($entityRegistry);
+
+        $this->dataMapperMock->expects($this->once())->method('getById')->willReturn($entityStub);
+
+        $id = 'foo';
+
+        $actualResult = $this->sut->getById($id);
+
+        $this->assertSame($entityStub, $actualResult);
+    }
+
+    public function testAdd()
+    {
+        $entityStub = new Entity('foo0', 'foo-0', null, null);
+
+        $this->unitOfWorkMock->expects($this->once())->method('scheduleForInsertion')->with($entityStub);
+
+        $this->sut->add($entityStub);
+    }
+
+    public function testDelete()
+    {
+        $entityStub = new Entity('foo0', 'foo-0', null, null);
+
+        $this->unitOfWorkMock->expects($this->once())->method('scheduleForDeletion')->with($entityStub);
+
+        $this->sut->delete($entityStub);
+    }
+
+    public function testGetByClientId()
+    {
+        $identifier = 'foo-0';
+        $entityStub = new Entity('foo0', $identifier, null, null);
+
+        $entityRegistry = $this->createEntityRegistryStub(null);
+
+        $this->dataMapperMock->expects($this->once())->method('getByClientId')->willReturn($entityStub);
+
+        $this->unitOfWorkMock->expects($this->any())->method('getEntityRegistry')->willReturn($entityRegistry);
+
+        $actualResult = $this->sut->getByClientId($identifier);
+
+        $this->assertSame($entityStub, $actualResult);
+    }
+
+    /**
+     * @param Entity|null $entity
+     *
+     * @return MockObject
+     */
+    protected function createEntityRegistryStub(?Entity $entity): MockObject
+    {
+        $entityRegistry = $this->getMockBuilder(IEntityRegistry::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods([
+                'clear',
+                'clearAggregateRoots',
+                'deregisterEntity',
+                'getClassName',
+                'getEntities',
+                'getEntity',
+                'getEntityState',
+                'getObjectHashId',
+                'isRegistered',
+                'registerAggregateRootCallback',
+                'registerEntity',
+                'runAggregateRootCallbacks',
+                'setState',
+            ])
+            ->getMock();
+
+        $entityRegistry->expects($this->any())->method('registerEntity');
+        $entityRegistry->expects($this->any())->method('getEntity')->willReturn($entity);
+
+        return $entityRegistry;
+    }
+}
