bb015e3837791539d4d5619535d6878e87d1c4c2
Peter Aba <peter@peteraba.com>
2019-08-20 13:40:09 +0200

    Add tests for LastGridPage middleware

---
+++ tests/Http/Middleware/LastGridPageTest.php
@@ -0,0 +1,107 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AbterPhp\Admin\Http\Middleware;
+
+use Opulence\Http\Requests\Request;
+use Opulence\Http\Responses\Response;
+use Opulence\Sessions\ISession;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+class LastGridPageTest extends TestCase
+{
+    /** @var LastGridPage - System Under Test */
+    protected $sut;
+
+    /** @var MockObject|ISession */
+    protected $sessionMock;
+
+    public function setUp(): void
+    {
+        $this->sessionMock = $this->getMockBuilder(ISession::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods(
+                [
+                    'ageFlashData',
+                    'delete',
+                    'flash',
+                    'flush',
+                    'get',
+                    'getAll',
+                    'getId',
+                    'getName',
+                    'has',
+                    'hasStarted',
+                    'reflash',
+                    'regenerateId',
+                    'set',
+                    'setId',
+                    'setMany',
+                    'setName',
+                    'start',
+                    'offsetExists',
+                    'offsetGet',
+                    'offsetSet',
+                    'offsetUnset',
+                ]
+            )
+            ->getMock();
+
+        $this->sut = new LastGridPage(
+            $this->sessionMock
+        );
+    }
+
+    public function testHandleDoesNotModifySessionIfResponseIsError()
+    {
+        $this->sessionMock->expects($this->never())->method('set');
+
+        $requestStub  = new Request([], [], [], [], [], [], null);
+        $responseStub = new Response();
+        $responseStub->setStatusCode(rand(400, 600));
+
+        $next = function () use ($responseStub) {
+            return $responseStub;
+        };
+
+        $actualResult = $this->sut->handle($requestStub, $next);
+
+        $this->assertSame($responseStub, $actualResult);
+    }
+
+    public function testHandleDoesNotModifySessionIfSessionIsNotInitialized()
+    {
+        $this->sessionMock->expects($this->once())->method('has')->willReturn(false);
+        $this->sessionMock->expects($this->never())->method('set');
+
+        $requestStub  = new Request([], [], [], [], [], [], null);
+        $responseStub = new Response();
+
+        $next = function () use ($responseStub) {
+            return $responseStub;
+        };
+
+        $actualResult = $this->sut->handle($requestStub, $next);
+
+        $this->assertSame($responseStub, $actualResult);
+    }
+
+    public function testHandleModifiesSessionIfSessionIsInitialized()
+    {
+        $this->sessionMock->expects($this->any())->method('has')->willReturn(true);
+        $this->sessionMock->expects($this->once())->method('set');
+
+        $requestStub  = new Request([], [], [], [], [], [], null);
+        $responseStub = new Response();
+
+        $next = function () use ($responseStub) {
+            return $responseStub;
+        };
+
+        $actualResult = $this->sut->handle($requestStub, $next);
+
+        $this->assertSame($responseStub, $actualResult);
+    }
+}
